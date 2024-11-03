abec347fc42e717204df8ba6370bf96e9284c553
Tanisha Misra <tanisham426@gmail.com>
2024-11-04 00:45:24 +0530

    Update client directory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//Components
+import Navbar from './components/Navbar/Navbar';
+
+//Pages
+import Home from './pages/Home/Home';
+import Books from './pages/Books/Books';
+import AddBook from './pages/Books/AddBook';
+import EditBook from './pages/Books/EditBook';
+
+//CSS
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books' element={<Books />} />
+        <Route path='/addBook' element={<AddBook />} />
+        <Route path='/books/editBook/:id' element={<EditBook />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
